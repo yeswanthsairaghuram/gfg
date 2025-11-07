16da16452971e7bd2b9a4bbfffdb13a36da2befb
Pydeti Yeswanth Sai RaghuRam <119839691+yeswanthsairaghuram@users.noreply.github.com>
2025-11-07 21:19:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Concatenate Integers/concatenate-integers.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        String str1=Integer.toString(a);
+        String str2=Integer.toString(b);
+        System.out.println(str1+str2);
+        // Write Your Code to Concatenate and print
+    }
+}
